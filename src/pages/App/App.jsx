66741bc66749be350bba7a0b6d4446209adc5fe3
ali-abadi <ali0.abadi0@gmail.com>
2023-09-19 10:35:33 +0300

    Add Helmet

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,7 +1,13 @@
+import { Helmet } from "react-helmet";
 import { Routes, Route } from "react-router-dom";
 export default function App() {
   return (
     <>
+      {/* React Helmet: React Helmet enables you to dynamically update the document head (e.g., title, meta tags) of your React app. This is useful for managing SEO-related elements or injecting external scripts into your app. */}
+      <Helmet>
+        <title>My App</title>
+        <meta name="description" content="A React app" />
+      </Helmet>
       <Routes>
         <Route path="/" element={<h1>App</h1>} />
         <Route path="*" element={<h1>Not Found</h1>} />
